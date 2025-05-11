7731d38893a0fa0811fe334a81e4c44a872ebf47
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-05-11 10:34:17 +0200

    feat: init server, gitignore and env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-(async () => {
-  console.log('index.ts: esm-pure-experimental-strip-types')
-  const text:string = 'Hello, world!'
-  console.log(text)
-})();
+// (async () => {
+//   console.log('index.ts: esm-pure-experimental-strip-types')
+//   const text:string = 'Hello, world!'
+//   console.log(text)
+// })();
+import http from 'node:http';
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  if (req.method === "GET" && req.url === "/api/users") {   
+    res.writeHead(200);
+    const users = [
+      { id: 11, name: 'Jesper' },
+      { id: 21, name: 'Karen' },
+    ];
+    return res.end(JSON.stringify(users));
+  }
+  res.end(JSON.stringify({
+    data: 'This is node CRUD API with watch and Typescript!',
+  }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log('index.ts: esm-pure-experimental-strip-types');
+});
+
+// (async () => {
+//   console.log('index.ts: esm-pure-experimental-strip-types')
+//   const text:string = 'Hello, world!'
+//   console.log(text)
+
+// })();
+
